4cf56339c26aff75347e3b006f1f4103fce8dafa
Vadim Flaks <Max@Vadims-MacBook-Pro.local>
2019-02-18 09:47:01 -0600

    first rough draft

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+
+function goToBookmark(address) {
+    let dir = document.getElementById(address);
+    dir.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+}
+
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    // window.onbeforeunload = function () {
+    //     window.scrollTo(0, 0);
+    // }
+    let projects = {
+        sb: {
+            dom: document.getElementById("swordbuilder"),
+            url: "http://18.223.203.241",
+        },
+        galleass: {
+            dom: document.getElementById("galleass"),
+            url: "https://github.com/MVanderer/Galleass",
+        },
+        skillet: {
+            dom: document.getElementById("skillet"),
+            url: "http://13.59.185.108/",
+        },
+    };
+
+    for (let key in projects) {
+        projects[key].dom.addEventListener("touchend", (e) => {
+            goToProjPage(projects[key].dom, projects[key].url);
+        });
+        projects[key].dom.addEventListener("click", (e) => {
+            goToProjPage(projects[key].dom, projects[key].url);
+        });
+    }
+
+    function goToProjPage(domElem, url) {
+        if (window.innerWidth > window.innerHeight) {
+            if (domElem.clientWidth >= Math.floor(window.innerWidth * 0.75)) {
+                window.location.href = url;
+            };
+        }
+        else {
+            if (domElem.clientHeight >= Math.floor(domElem.parentElement.parentElement.clientHeight * 0.75)) {
+                window.location.href = url;
+            };
+
+        }
+    }
+});
+
+
